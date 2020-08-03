02163f9d719ebb9adfbc8fd4e650c7af34d24197
林孟慧 <linmenghui@qiyi.com>
2020-08-03 19:31:32 +0800

    add frontend validation and throttle

--- nginx/static/js/button.js
+++ nginx/static/js/button.js
@@ -1,30 +1,40 @@
 const btn = document.querySelector('button');
 
 function shortenURL() {
+    const url_max_len = 200;
+    const url_to_shorten = document.getElementById('url').value;
+    const url_len = url_to_shorten.length;
+
+    if (url_len == 0 || url_len > url_max_len) {
+        document.getElementById('msg').innerHTML = "Please enter url (at most 200 characters)";
+        return;
+    }
+
     const xhr = new XMLHttpRequest();
     const url = "http://localhost/api/shorten";  // TODO: put in config
     xhr.open('POST', url, true);
     xhr.setRequestHeader("Content-type", "application/json");
 
-    const url_to_shorten = document.getElementById('url').value
-    const payload = JSON.stringify({"url": url_to_shorten})
-
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = () => {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            data = JSON.parse(xhr.responseText)
-            var msg = ""
+            if (xhr.status == 503) {
+                document.getElementById('msg').innerHTML =
+                    "Your quota for shortening/redirecting URL service is over now.\n Please try it later.";
+                return;
+            }
+
+            data = JSON.parse(xhr.responseText);
             if (xhr.status == 400) {
                 document.getElementById('msg').innerHTML = data['msg'];
             } else {
-                const shorten_url = data['shorten_url']
+                const shorten_url = data['shorten_url'];
                 document.getElementById('msg').innerHTML =
                     url_to_shorten + " is converted to <a target='_blank' rel='noopener noreferrer' href='" + shorten_url + "'>" + shorten_url + "</a>";
             }
         }
     }
-    xhr.send(payload)
+    const payload = JSON.stringify({"url": url_to_shorten});
+    xhr.send(payload);
 }
 
-btn.addEventListener('click', function(){
-    shortenURL();
-})
+btn.addEventListener('click', shortenURL);
